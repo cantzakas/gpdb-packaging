ebd75991278f141736a87ddfd39f214916a51f9b
cantzakas <cantzakas@pivotal.io>
2019-04-30 09:47:47 +0100

    changes to accomodate X.Y.Z-beta.N numbering

--- helpers/pivnet.sh
+++ helpers/pivnet.sh
@@ -54,7 +54,7 @@ get_pivnet_product_releases() {
   local RELEASES_URL="$1"
   local RECORD_COUNT="$2"
   get_pivnet_data "$RELEASES_URL" \
-    | jq -r '.releases | sort_by(.version | split(".") | map(tonumber))[].version' \
+    | jq -r '.releases | sort_by(.version | split("."))[].version' \
     | tail -r -n"$RECORD_COUNT"
 }
 
